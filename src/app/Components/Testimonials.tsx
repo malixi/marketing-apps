af22078647bf4109e39e88a4187280ddec4902bb
Carlo <malixi@users.noreply.github.com>
2024-06-08 16:52:09 -0700

    styling updates

--- src/app/Components/Testimonials.tsx
+++ src/app/Components/Testimonials.tsx
@@ -1,9 +1,9 @@
 const Testimonial = ({ data } : { data: { name: string, username: string, image: string, description: string } }) => {
     return (
-        <div className="max-w-[21.5rem] rounded bg-white overflow-hidden shadow-lg">
-            <div className="px-6 py-4 text-neutral-600">
+        <div className="max-w-[21.5rem] rounded-lg bg-white overflow-hidden shadow-lg">
+            <div className="px-4 py-5 text-neutral-600">
                 <div className="flex flex-row">
-                    <img className="w-12 h-12 mt-2 mr-3 rounded-[50px] shadow-lg" src={data.image}/>
+                    <img className="w-12 h-12 mt-2 mr-2 rounded-[50px] shadow-lg" src={data.image}/>
                     <div className="flex flex-col mt-2">
                         <p className="font-semibold text-lg text-neutral-900">{data.name}</p>    
                         <p className="text-sm " >@{data.username}</p>
