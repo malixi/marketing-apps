a4b5c228021326e234de6675cf8b2e7236479e3d
Carlo <malixi@users.noreply.github.com>
2024-06-08 23:16:52 -0700

    Updated card styling

--- src/app/Components/Testimonials.tsx
+++ src/app/Components/Testimonials.tsx
@@ -1,10 +1,10 @@
 const Testimonial = ({ data } : { data: { name: string, username: string, image: string, description: string } }) => {
     return (
         <div className="max-w-[21.5rem] rounded-lg bg-white overflow-hidden shadow-lg">
-            <div className="px-4 py-5 text-neutral-600">
+            <div className="px-6 py-5 text-neutral-600">
                 <div className="flex flex-row">
-                    <img className="w-12 h-12 mt-2 mr-2 rounded-[50px] shadow-lg" src={data.image}/>
-                    <div className="flex flex-col mt-2">
+                    <img className="w-12 h-12 mt-1 mr-1 rounded-[50px] shadow-lg" src={data.image}/>
+                    <div className="flex flex-col mt-1 ml-3">
                         <p className="font-semibold text-lg text-neutral-900">{data.name}</p>    
                         <p className="text-sm " >@{data.username}</p>
                     </div>
