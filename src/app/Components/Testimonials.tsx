f2702eb1ca0c0ec637ebf0a8b9aa1e72b9a94c72
Carlo <malixi@users.noreply.github.com>
2024-06-09 00:12:56 -0700

    updated card

--- src/app/Components/Testimonials.tsx
+++ src/app/Components/Testimonials.tsx
@@ -1,11 +1,11 @@
 const Testimonial = ({ data } : { data: { name: string, username: string, image: string, description: string } }) => {
     return (
-        <div className="max-w-[21.5rem] rounded-lg bg-white overflow-hidden shadow-lg">
-            <div className="px-6 py-5 text-neutral-600">
+        <div className="max-w-[21.5rem] rounded-lg bg-white overflow-hidden shadow">
+            <div className="px-[1.5em] py-5 text-neutral-600">
                 <div className="flex flex-row">
                     <img className="w-12 h-12 mt-1 mr-1 rounded-[50px] shadow-lg" src={data.image}/>
                     <div className="flex flex-col mt-1 ml-3">
-                        <p className="font-semibold text-lg text-neutral-900">{data.name}</p>    
+                        <p className="whitespace-nowrap font-semibold text-lg text-neutral-900">{data.name}</p>    
                         <p className="text-sm " >@{data.username}</p>
                     </div>
                 </div>
