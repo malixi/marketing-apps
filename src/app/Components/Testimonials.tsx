b5ffb68eb12778a15e71c458d5f6bece0f6d4454
Carlo <malixi@users.noreply.github.com>
2024-06-10 00:26:26 -0700

    Updated styling and layout

--- src/app/Components/Testimonials.tsx
+++ src/app/Components/Testimonials.tsx
@@ -2,11 +2,11 @@ const Testimonial = ({ data } : { data: { name: string, username: string, image:
     return (
         <div className="max-w-[21.5rem] rounded-lg bg-white overflow-hidden shadow">
             <div className="px-[1.5em] py-5 text-neutral-600">
-                <div className="flex flex-row">
-                    <img className="w-12 h-12 mt-1 mr-1 rounded-[50px] shadow-lg" src={data.image}/>
-                    <div className="flex flex-col mt-1 ml-3">
-                        <p className="whitespace-nowrap font-semibold text-lg text-neutral-900">{data.name}</p>    
-                        <p className="text-sm " >@{data.username}</p>
+                <div className="flex flex-row mt-1">
+                    <img className="size-12 mr-4 rounded-[50px] shadow-lg" src={data.image} />
+                    <div className="flex flex-col">
+                    <p className="whitespace-nowrap font-semibold text-lg text-neutral-900">{data.name}</p>    
+                    <p className="text-sm">@{data.username}</p>
                     </div>
                 </div>
                 <p className="text-base mt-4">
