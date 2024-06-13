a08f55b3aeb62f8126f79141169168b673a041b4
Carlo <malixi@users.noreply.github.com>
2024-06-12 23:09:16 -0700

    styling updates

--- src/app/pages/testimonial/TestimonialCard.tsx
+++ src/app/pages/testimonial/TestimonialCard.tsx
@@ -3,19 +3,17 @@ import './style.css';
 
 const TestimonialCard = ({ data } : { data: { name: string, username: string, image: string, description: string } }) => {
     return (
-        <div className="max-w-[21.5rem] rounded-lg bg-white overflow-hidden shadow-sm">
-            <div className="px-[1.5em] py-5 text-neutral-600">
-                <div className="flex flex-row mt-1">
-                    <img className="size-12 mr-4 rounded-[50px] shadow-lg" src={data.image} />
+        <div className="mx-auto w-[340px] flex flex-col p-6 gap-4 border border-solid border-neutral-200 rounded-lg bg-white shadow-m text-neutral-600">
+                <div className="flex items-center gap-4 w-full">
+                    <img className="size-12 rounded-[50px] shadow-lg" src={data.image} />
                     <div className="flex flex-col">
                     <p className="whitespace-nowrap font-semibold text-lg text-neutral-900">{data.name}</p>    
                     <p className="text-sm">@{data.username}</p>
                     </div>
                 </div>
-                <p className="text-base mt-4">
+                <p className="text-base w-full">
                    {data.description}
                 </p>
-            </div>
         </div>
     )
 }
--- src/app/pages/testimonial/page.tsx
+++ src/app/pages/testimonial/page.tsx
@@ -4,7 +4,7 @@ const data = {
   name: "Sarah Dole",
   username: "sarahdole",
   image: "/profile-thumbnail.png",
-  description: "I've been searching for high quality abstract images for my design projects, and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!"
+  description: "I've been searching for high-quality abstract images for my design projects, and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!"
 }
 
 const TestimonialPage = () => {
