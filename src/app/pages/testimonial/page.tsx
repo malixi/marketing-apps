7fed6b4557fc499c6ae23b1411d76111b6a44234
Carlo <malixi@users.noreply.github.com>
2024-06-13 13:31:05 -0700

    updated the styling

--- src/app/pages/testimonial/page.tsx
+++ src/app/pages/testimonial/page.tsx
@@ -10,23 +10,23 @@ const data = {
 
 const TestimonialPage = () => {
   return (
-    // <div className="h-screen w-screen bg-gradient-to-r from-[#F9FAFB] to-[#D2D6DB] flex flex-col">
-    //   <div className="flex flex-col items-center mt-[200px]">
-    //   <TestimonialCard data={data} /></div>
-    //   <div className="credits" data-gfe-screenshot-exclude="true">
-    //   A challenge by 
-    //   <a
-    //     href="https://www.greatfrontend.com/projects?ref=challenges"
-    //     target="_blank"
-    //     > GreatFrontEnd Projects</a
-    //   >. Built by
-    //   <a
-    //     href="https://www.greatfrontend.com/projects/u/username"
-    //     target="_blank"
-    //     > Carlo</a
-    //   >.
-    // </div>
-    // </div>
+    <div className="h-screen w-screen bg-gradient-to-r from-[#F9FAFB] to-[#D2D6DB] flex flex-col">
+      {/* <div className="flex flex-col items-center mt-[200px]">
+      <TestimonialCard data={data} /></div>
+      <div className="credits" data-gfe-screenshot-exclude="true">
+      A challenge by 
+      <a
+        href="https://www.greatfrontend.com/projects?ref=challenges"
+        target="_blank"
+        > GreatFrontEnd Projects</a
+      >. Built by
+      <a
+        href="https://www.greatfrontend.com/projects/u/username"
+        target="_blank"
+        > Carlo</a
+      >.
+    </div> */}
+    </div>
   );
 }
 
